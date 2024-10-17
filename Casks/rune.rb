33fa50403a6d72bcbcab3fbdb5978e27ae56cdd0
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-10-17 22:09:31 +0200

    init

---
+++ Casks/rune.rb
@@ -0,0 +1,19 @@
+cask "rune@nightly" do
+    arch arm: "aarch64", intel: "x64"
+  
+    version :latest
+    sha256 :no_check
+      
+    url "https://github.com/Losses/rune/releases/download/v0.0.0-dev.13/Rune--5c00afa-macOS.dmg"
+    name "Rune"
+    desc "The nightly version of rune player."
+    homepage "https://github.com/Losses/rune"
+  
+    app "Rune.app"
+    binary "#{appdir}/Rune.app/Contents/MacOS/Rune"
+  
+    zap trash: [
+        
+    ]
+  end
+  
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # homebrew-rune
+
+## Install with brew
+
+- Add rune:
+
+```bash
+brew tap XMLHexagram/rune
+```
+
+- Install nightly version rune:
+
+```bash
+brew install --cask rune@nightly
+```
+
+- Upgrade nightly version rune
+
+```bash
+brew upgrade --cask rune@nightly
+```
