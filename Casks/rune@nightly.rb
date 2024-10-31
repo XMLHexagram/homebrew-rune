34a049ba6f866e465c6b2f28bc920b0534b50d15
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-10-31 16:57:51 +0100

    Update rune@nightly.rb

--- Casks/rune@nightly.rb
+++ Casks/rune@nightly.rb
@@ -4,7 +4,7 @@ cask "rune@nightly" do
     version :latest
     sha256 :no_check
       
-    url "https://github.com/Losses/rune/releases/download/v0.0.0-dev.17/Rune-v0.0.0-dev.17-f30f4fa-macOS.dmg"
+    url "https://github.com/Losses/rune/releases/download/v0.0.0-dev.19/Rune-v0.0.0-dev.19-954aff9-macOS.dmg"
     name "Rune"
     desc "The nightly version of rune player."
     homepage "https://github.com/Losses/rune"
