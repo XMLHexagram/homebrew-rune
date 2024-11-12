bf75fa04be4afaf373e59db638a10c8c15c81d96
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-11-12 16:26:06 +0100

    Update rune@alpha.rb

--- Casks/rune@alpha.rb
+++ Casks/rune@alpha.rb
@@ -4,7 +4,7 @@ cask "rune@alpha" do
     version :latest
     sha256 :no_check
       
-    url "https://github.com/Losses/rune/releases/download/v0.0.0-alpha.1/Rune-v0.0.0-alpha.1-b24587b-macOS.dmg"
+    url "https://github.com/Losses/rune/releases/download/v0.0.0-alpha.2/Rune-v0.0.0-alpha.2-2a816c6-macOS.dmg"
     name "Rune"
     desc "The alpha version of rune player."
     homepage "https://github.com/Losses/rune"
