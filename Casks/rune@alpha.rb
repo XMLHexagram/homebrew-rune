991923946f42a973b5308f87b5f7bc5d1e4f1389
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-11-25 16:26:28 +0100

    Update rune@alpha.rb

--- Casks/rune@alpha.rb
+++ Casks/rune@alpha.rb
@@ -1,7 +1,7 @@
 cask "rune@alpha" do
     arch arm: "aarch64", intel: "x64"
   
-    version "v0.0.0-alpha.10"
+    version "v0.0.0-alpha.12"
     sha256 :no_check
       
     url "https://github.com/Losses/rune/releases/download/v0.0.0-alpha.12/Rune-v0.0.0-alpha.12-03eead4-macOS.dmg"
