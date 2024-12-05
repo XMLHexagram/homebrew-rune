a8688519d822596599e6441592ca21cd0f6891f7
XMLHexagram <i@hexagr.am>
2024-12-05 17:59:48 +0100

    rune@beta 1.0.0-beta.3

--- Casks/rune@beta.rb
+++ Casks/rune@beta.rb
@@ -1,20 +1,20 @@
 cask "rune@beta" do
-  version "1.0.0-beta.2"
-  sha256 "f31a8abcf83e9996c119eb4ec330778d9ab327aa029338cd815459729a4100de"
+  version "1.0.0-beta.3"
+  sha256 "85fdcac3c0c63cc0de74ea542e2e98b2129c4576abe949eb7f61511465ad8145"
+
+  url "https://github.com/Losses/rune/releases/download/v#{version}/Rune-v#{version}-macOS.zip"
+  name "Rune"
+  desc "Experience timeless melodies with a music player"
+  homepage "https://github.com/Losses/rune"
 
   livecheck do
     url :url
     strategy :github_latest
     regex(/^v?(\d+(?:\.\d+)+(?:-beta\.\d+)?)$/)
   end
-  
-  url "https://github.com/Losses/rune/releases/download/v#{version}/Rune-v#{version}-macOS.zip"
-  name "Rune"
-  desc "Experience timeless melodies with a music player"
-  homepage "https://github.com/Losses/rune"
 
   auto_updates true
-  
+
   app "Rune.app", target: "Rune@beta.app"
   binary "#{appdir}/Rune.app/Contents/MacOS/Rune"
 
