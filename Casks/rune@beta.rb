ec302e50ad144c9c91e2d3492df93e3a92c0825c
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-12-05 17:58:33 +0100

    Update rune@beta.rb

--- Casks/rune@beta.rb
+++ Casks/rune@beta.rb
@@ -1,6 +1,4 @@
 cask "rune@beta" do
-  arch arm: "aarch64", intel: "x64"
-
   version "1.0.0-beta.2"
   sha256 "f31a8abcf83e9996c119eb4ec330778d9ab327aa029338cd815459729a4100de"
 
@@ -12,7 +10,7 @@ cask "rune@beta" do
   
   url "https://github.com/Losses/rune/releases/download/v#{version}/Rune-v#{version}-macOS.zip"
   name "Rune"
-  desc "Experience timeless melodies with a music player that blends classic design with modern technology."
+  desc "Experience timeless melodies with a music player"
   homepage "https://github.com/Losses/rune"
 
   auto_updates true
@@ -22,7 +20,6 @@ cask "rune@beta" do
 
   zap trash: [
     "/Users/hexagram/Library/Application Scripts/ci.not.rune",
-    "/Users/hexagram/Library/Containers/ci.not.rune"
+    "/Users/hexagram/Library/Containers/ci.not.rune",
   ]
 end
-  
