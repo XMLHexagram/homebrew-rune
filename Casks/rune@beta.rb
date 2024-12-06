844d103d9bfc57d64b4de2ec86ff72b3708d67e7
XMLHexagram <i@hexagr.am>
2024-12-06 15:57:52 +0100

    rune@beta 1.0.0-beta.4

--- Casks/rune@beta.rb
+++ Casks/rune@beta.rb
@@ -1,6 +1,6 @@
 cask "rune@beta" do
-  version "1.0.0-beta.3"
-  sha256 "85fdcac3c0c63cc0de74ea542e2e98b2129c4576abe949eb7f61511465ad8145"
+  version "1.0.0-beta.4"
+  sha256 "a69ced3ce781c8bc6d0752264608271c368ba5fb6850379e8ed3394da2f2a6be"
 
   url "https://github.com/Losses/rune/releases/download/v#{version}/Rune-v#{version}-macOS.zip"
   name "Rune"
